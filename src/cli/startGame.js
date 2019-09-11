606c1e012f65059edc464768ab9184de0d360c2f
Dugi <me@dukagj.in>
2019-09-11 14:02:39 +0200

    Refactors startGame function.

--- src/cli/startGame.js
+++ src/cli/startGame.js
@@ -4,16 +4,16 @@ const inquirer = require('inquirer');
 const Choice = require('../field/choice');
 
 const startGame = (matrix, players) => {
-  let playerTurn = 1;
-  let playerColor = 'blue';
+  let whoseTurn = 1;
+  let turnColor = 'blue';
 
   const chosenFields = [];
   const fieldPrompt = async () => {
-    const field = await inquirer.prompt([
+    const turn = await inquirer.prompt([
       {
         type: 'list',
         name: 'player_choice',
-        message: chalk[playerColor](`Choose a field (Player ${playerTurn} / ${players[playerTurn].name}): `),
+        message: chalk[turnColor](`Choose a field (Player ${whoseTurn} / ${players[whoseTurn].name}): `),
         choices: () =>
           Choice.possibleChoices
             .filter(field => !chosenFields.includes(field))
@@ -23,14 +23,14 @@ const startGame = (matrix, players) => {
     ]);
 
     try {
-      matrix.addChoice(new Choice(field.player_choice, players[playerTurn]));
-      chosenFields.push(field.player_choice.toLowerCase());
+      matrix.addChoice(new Choice(turn.player_choice, players[whoseTurn]));
+      chosenFields.push(turn.player_choice.toLowerCase());
     } catch (err) {
       console.error(err);
     }
 
-    playerTurn = playerTurn === 1 ? 2 : 1;
-    playerColor = playerTurn === 1 ? 'blue' : 'red';
+    whoseTurn = whoseTurn === 1 ? 2 : 1;
+    turnColor = whoseTurn === 1 ? 'blue' : 'red';
 
     clear();
 
